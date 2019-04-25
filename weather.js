c1bf6bfc36f22b2e42600b6186a06343635a5287
naviroyal <naviroyal909098@gmail.com>
2019-04-25 11:55:35 +0530

    Final commit

--- weather.js
+++ weather.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
                 });           
             }
         else{
-            $('#error').html('Field cant empty');
+            $('#error').html("<div id='errorCity' style='margin-top:80px;' class='alert alert-danger alert-dismissible'><button type='button' class='close' data-dismiss='alert'>&times;</button>Field Cannot be empty</div>");
         }
         
     });    
@@ -25,13 +25,13 @@ $(document).ready(function(){
 
 function show(data){
     return "<h3 style='font-size:40px;font-weight:bold;' class='text-center'>Current Weather for "+ data.name +", "+ data.sys.country +"</h3>"+
-    "<h3><strong>Weather</strong>:"+ data.weather[0].main +"</h3>" + 
-        "<h3><strong>Description</strong>:"+ data.weather[0].description +"</h3>" +
-        "<h3><strong>Temperature</strong>:"+ data.main.temp +"</h3>"+
-        "<h3><strong>Pressure</strong>:"+ data.main.pressure +"</h3>" +
-        "<h3><strong>Humidity</strong>:"+ data.main.humidity +"</h3>" +
-        "<h3><strong>Min. Temperature</strong>:"+ data.main.temp_min +"</h3>" +
-        "<h3><strong>Max. Temperature</strong>:"+ data.main.temp_max +"</h3>" +
-        "<h3><strong>Wind Speed</strong>:"+ data.wind.speed +"</h3>" +
-        "<h3><strong>Wind Direction</strong>:"+ data.wind.deg +"</h3>";
+    "<h3 style='padding-left:40px;'><strong>Weather</strong>:"+ data.weather[0].main +"</h3>" + 
+        "<h3 style='padding-left:40px;'><strong>Description</strong>:<img src='http://openweathermap.org/img/w/"+data.weather[0].icon+".png'>"+data.weather[0].description +"</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Temperature</strong>:"+ data.main.temp +"&deg;C</h3>"+
+        "<h3 style='padding-left:40px;'><strong>Pressure</strong>:"+ data.main.pressure +" hPa</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Humidity</strong>:"+ data.main.humidity +"%</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Min. Temperature</strong>:"+ data.main.temp_min +"&deg;C</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Max. Temperature</strong>:"+ data.main.temp_max +"&deg;C</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Wind Speed</strong>:"+ data.wind.speed +" m/s</h3>" +
+        "<h3 style='padding-left:40px;'><strong>Wind Direction</strong>:"+ data.wind.deg +"&deg;</h3>";
 }
